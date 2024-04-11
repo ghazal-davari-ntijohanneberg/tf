503eb63800c1bc4c10d9aac2532fbdb161063b6e
ghazal-davari-ntijohanneberg <118273686+ghazal-davari-ntijohanneberg@users.noreply.github.com>
2024-04-11 11:42:40 +0200

    .

--- app.rb
+++ app.rb
@@ -11,11 +11,19 @@ get('/') do
 end
 
 get('/register') do
-    slim(:register)
+    slim(:"login/register")
 end
 
 get('/showlogin') do
-    slim(:login)
+    slim(:"login/login")
+end
+
+get('/special') do
+    if session[:username]
+        slim(:special)
+    else 
+        "You must be logged in to view this page"
+    end
 end
   
 post('/login') do
@@ -56,7 +64,7 @@ post('/users/new') do
     existing_user = db.execute('SELECT * FROM Users WHERE Username = ?', username).first
 
     if existing_user
-        slim(:signup_error)
+        slim(:"login/signup_error")
     elsif password != password_confirm
         slim (:password_mismatch_error)
     else
@@ -72,7 +80,6 @@ get ("/offers") do
 end
 
 post ('/add_to_travel_plan') do
-    puts "Session ID: #{session[:user_id]}"
     if session[:user_id].nil?
         puts "Redirecting to login page" 
         redirect '/showlogin'
@@ -83,8 +90,15 @@ post ('/add_to_travel_plan') do
         db = SQLite3::Database.new('db/wsprojekt.db')
         db.execute("INSERT INTO Travels (country_id, user_id) VALUES (?, ?)", country_id, user_id)
     
-        redirect '/mypage'
+        redirect '/offers'
     end
+end
+
+post('/Travels/:country_id/delete') do
+    country_id = params[:country_id].to_i
+    db = SQLite3::Database.new('db/wsprojekt.db')
+    db.execute("DELETE FROM Travels WHERE country_id = ?", country_id)
+    redirect '/mypage'
 end
 
 get '/logout' do
